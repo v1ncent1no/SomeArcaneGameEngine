d69688c181b49e5ed4a63de753e2c043852903c6
Nikita Syrman <nikita.livesey@gmail.com>
2023-12-18 00:04:47 +0300

    working on custom memory allocators
    Right now made Arena Alloctor, but didn't test it right now. Also, gonna rewrite it anyways, got a better idea to pass parent allocator threw kp_allocator_t interface

--- src/kp_memalloc.c
+++ src/kp_memalloc.c
@@ -1,19 +1,93 @@
 #include <assert.h>
 #include "kp_memalloc.h"
+#include "kp_macroutils.h"
+
+
+// FIXME: rewrite it, so that custom allocators use kp_allocator_t interface
+//        as a parant allocator;
+#ifndef KP_MEMALLOC_CUSTOM_MALLOC
+#include <malloc.h>
+
+#define KP_MALLOC(size)          malloc(size)
+#define KP_FREE(ptr)             free(ptr)
+#define KP_REALLOC(ptr, newsize) realloc(ptr, newsize)
+#endif
 
 /**
- * Initializes the arena. It's optional right now, as you may just do that
- * manually.
+ * @param `size` is a number of bytes to be allocated for one region
  */
-kp_memory_error_t kp_arena_init(kp_arena_t *arena, size_t regions_size) {
-    return KP_MEMORY_OK;
+static kp_memory_error_t kp_region_init(struct __kp_arena_region *region,
+                                        size_t size) {
+    region->raw_memory = KP_MALLOC(size);
+    if (!region->raw_memory)
+        return KP_MEMORY_ERROR_REGION_ALLOC;
+
+    region->local_size = size;
+    region->pointer = NULL;
+    region->next = NULL;
+
+    return KP_MEMORY_ERROR_OK;
+}
+
+/**
+ * Initializes the arena.
+ *
+ * @param `arena` is a pointer to arena
+ * @param `region_size`
+ */
+kp_memory_error_t kp_arena_init(kp_arena_t *arena, size_t region_size) {
+    arena->region_size = region_size;
+    arena->root = KP_MALLOC(sizeof(struct __kp_arena_region));
+
+    kp_memory_error_t err = kp_region_init(
+        arena->root,
+        MAX(KP_ARENA_MIN_REGION_SIZE, region_size)
+    );
+
+    if (!err)
+        return err;
+    return KP_MEMORY_ERROR_OK;
 }
 
 /**
  * Deletes a bunch of memory allocated by this exact kp_arena_t
  */
 kp_memory_error_t kp_arena_deinit(kp_arena_t* arena) {
-    return KP_MEMORY_OK;
+    if (!arena)
+        return KP_MEMORY_ERROR_ARENA_NOT_INITIALIZED;
+    if (!arena->root)
+        return KP_MEMORY_ERROR_REGION_NOT_INITIALIZED;
+
+    struct __kp_arena_region *region = arena->root;
+    while (region->next != NULL) {
+        struct __kp_arena_region *next = region->next;
+        KP_FREE(region);
+        region = next;
+    }
+
+    return KP_MEMORY_ERROR_OK;
+}
+
+static kp_memory_error_t kp_region_alloc_recursive(
+    struct __kp_arena_region* region,
+    size_t min_size,
+    size_t size,
+    void **ptr
+) {
+    if (!region) return KP_MEMORY_ERROR_REGION_NOT_INITIALIZED;
+
+    while (region->next)
+        region = region->next;
+
+    if ((region->local_size - (size_t)region->pointer) > size) {
+        region = KP_MALLOC(MAX(min_size, size));
+        if (!region) return KP_MEMORY_ERROR_REGION_ALLOC;
+    }
+
+    *ptr = region->pointer;
+    region->pointer += (size_t)region->pointer + size;
+
+    return KP_MEMORY_ERROR_OK;
 }
 
 /**
@@ -21,5 +95,16 @@ kp_memory_error_t kp_arena_deinit(kp_arena_t* arena) {
  * creates another and returns the address to int.
  */
 kp_memory_error_t kp_arena_alloc(kp_arena_t* arena, size_t size, void** ptr) {
-    return KP_MEMORY_OK;
+    if (!arena)
+        return KP_MEMORY_ERROR_ARENA_NOT_INITIALIZED;
+
+    kp_memory_error_t err = kp_region_alloc_recursive(
+        arena->root,
+        arena->region_size,
+        size,
+        ptr
+    );
+    if (!err) return err;
+
+    return KP_MEMORY_ERROR_OK;
 }
--- src/kp_memalloc.h
+++ src/kp_memalloc.h
@@ -7,21 +7,74 @@
  * 	- [ ] Arena Alocator (Working on it)
  * 	- [ ] Fixed Buffer Allocator
  * 	- [ ] (Safety on/off) Allocator
- *
  */
 
+/**
+ * `kp_alloctor_t` is unified memory allocator inteface.
+ *
+ * It's supposed to be passed with required methods:
+ *     - alloc(void *allocator, size_t size)
+ *     - free(void *allocator, void* ptr)
+ *     - realloc(void *allocator, void *ptr, size_t newsize)
+ */
+typedef void *(*kp_alloc_func_ptr)(void*, size_t);
+typedef void (*kp_free_func_ptr)(void*, void*);
+typedef void *(*kp_realloc_func_ptr)(void*, void*, size_t);
+
+typedef struct kp_alloctor {
+    void *parant_allocator;
+    kp_alloc_func_ptr alloc;
+    kp_realloc_func_ptr realloc;
+    kp_free_func_ptr free;
+} kp_alloctor_t;
+
+#ifndef KP_MALLOC_DEFAULT_INTEFACE
+#define KP_MALLOC_DEFAULT_INTEFACE
+
+#include <malloc.h>
+
+inline void *__kp_std_malloc(void* _, size_t size) {
+    return malloc(size);
+}
+inline void __kp_std_free(void* _, void* ptr) {
+    free(ptr);
+}
+inline void *__kp_std_realloc(void* _, void* ptr, size_t size) {
+    return realloc(ptr, size);
+}
+
+const kp_alloctor_t std_malloc_alloc = {
+    .parant_allocator = NULL,
+    .alloc = __kp_std_malloc,
+    .free = __kp_std_free,
+    .realloc = __kp_std_realloc
+};
+#endif // KP_MALLOC_DEFAULT_INTEFACE
+
 typedef enum kp_memory_error {
-    KP_MEMORY_OK
+    KP_MEMORY_ERROR_OK,
+    KP_MEMORY_ERROR_REGION_ALLOC,
+    KP_MEMORY_ERROR_ARENA_NOT_INITIALIZED,
+    KP_MEMORY_ERROR_REGION_NOT_INITIALIZED,
 } kp_memory_error_t;
 
 struct __kp_arena_region {
-    u8 *raw_memory;
+    void *raw_memory;
     size_t local_size;
-    size_t pointer;
+    void *pointer;
     struct __kp_arena_region *next;
 };
 
+#ifndef KP_ARENA_MIN_REGION_SIZE
+#define KP_ARENA_MIN_REGION_SIZE 1024 * 640 // 640K is Enough For Anyone
+#endif
+
+// FIXME: rewrite it, so that custom allocators use kp_allocator_t interface
+//        as a parant allocator;
+// NOTE: consider keeping a pointer to the last allocated region, which can give
+//       some *little pieace* of perfomance, as arena's deallocated all at once
 typedef struct kp_arena_allocator {
+    // kp_alloctor_t parent_alloc;
     size_t region_size;
     struct __kp_arena_region* root;
 } kp_arena_t;
@@ -30,14 +83,17 @@ typedef struct kp_arena_allocator {
  * Initializes the arena. It's optional right now, as you may just do that
  * manually.
  */
-kp_memory_error_t kp_arena_init(kp_arena_t *arena, size_t regions_size);
+kp_memory_error_t kp_arena_init(kp_arena_t *arena, size_t region_size);
 /**
  * Deletes a bunch of memory allocated by this exact kp_arena_t
  */
 kp_memory_error_t kp_arena_deinit(kp_arena_t* arena);
-
 /**
  * If there's enough space in the last region, return the address, otherwise
  * creates another and returns the address to int.
  */
 kp_memory_error_t kp_arena_alloc(kp_arena_t* arena, size_t size, void** ptr);
+/**
+ * No-Op for arena allocator.
+ */
+kp_memory_error_t kp_arena_dealloc(kp_arena_t* arena, size_t size, void** ptr);
